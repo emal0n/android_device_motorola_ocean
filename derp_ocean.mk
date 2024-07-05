7e5c9bd7855c712d79357dbc4b6209550c8db79f
emalon91 <edmundoneto511@hotmail.com>
2024-07-05 10:10:33 -0300

    ocean: Adjustments in derp goodies
    Change-Id: Ia3bf82b42638b002601b7728e6386efba1bb28f9

--- derp_ocean.mk
+++ derp_ocean.mk
@@ -6,6 +6,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+DERP_VERSION_APPEND_TIME_OF_DAY=true
+DERP_BUILDTYPE := Official
+TARGET_NOT_USES_BLUR=true
+USE_LEGACY_BOOTANIMATION := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_USES_PICO_GAPPS := true
+
 #BUILD_FINGERPRINT := motorola/ocean/ocean:10/QPOS30.52-29-12/401fd:user/release-keys
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := ocean
