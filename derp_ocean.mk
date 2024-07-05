312cd1319007f028642738fafa5b4bb1e6cd99e6
Vhmit <viktor.9630@protonmail.com>
2024-07-05 10:06:06 -0300

    ocean: DerpFest-ify 14
    Change-Id: I2ed1a7b2aa7f267333f43207c372c81163199c89
    Co-authored-by: emalon91 <edmundoneto511@hotmail.com>

--- derp_ocean.mk
+++ derp_ocean.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -11,7 +11,7 @@ PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := ocean
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := moto g(7) power
-PRODUCT_NAME := lineage_ocean
+PRODUCT_NAME := derp_ocean
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=ocean \
